d01de2caaaa149a5c8332651c714fef0382110c1
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-21 19:12:09 +0300

    init

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-    res.status(404).json({ message: "Not Found"})
+    res.status(404).json({ message: "Not Found" });
 });
 
 app.listen(PORT, () => {
