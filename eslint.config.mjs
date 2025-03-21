23f448cb7bbdd74f84f2c614112d76ffb0873348
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-21 10:46:51 +0300

    first configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
+import pluginJs from '@eslint/js';
 
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-]);
+  
+export default [
+    pluginJs.configs.recommended,
+    {
+      files: ['src/**/*.js'],
+      languageOptions: { globals: globals.node },
+      rules: { 
+          semi: 'error', 
+          'no-unused-vars': ['error', { args: 'none' }], 
+          'no-undef': 'error' 
+        },
+    },
+  ];
+  
