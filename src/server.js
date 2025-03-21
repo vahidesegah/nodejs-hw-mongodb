d72390315d409eae3136bf43b2c4ec3c30b51f14
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-21 12:10:32 +0300

    server.js / index.js

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import pinoHttp from "pino-http";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+
+export function setupServer() {
+
+dotenv.config();
+const PORT = process.env.PORT; //.env deki port u al
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(
+    pinoHttp({
+        transport: {
+            target: "pino-pretty",
+        },
+    }),
+);
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+};
