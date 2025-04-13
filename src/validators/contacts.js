74ed1f37870bd1ba0aff77c87b219aec91e04edb
Vahide Segah Üney <vahidesegah@gmail.com>
2025-04-13 22:14:33 +0300

    df

--- src/validators/contacts.js
+++ src/validators/contacts.js
@@ -9,8 +9,8 @@ export const createContactSchema = Joi.object({
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  phoneNumber: Joi.string().min(6).max(16).required(),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string(),
   isFavorite: Joi.boolean().default(false),
   contactType: Joi.string().valid('work', 'home', 'personal').default('personal'),
