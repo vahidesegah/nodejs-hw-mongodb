0ee908ffe283142d8d48d3e62ee04dc7d470fc06
Vahide Segah Üney <vahidesegah@gmail.com>
2025-04-13 21:58:26 +0300

    upd Schema vaL

--- src/validators/contacts.js
+++ src/validators/contacts.js
@@ -8,3 +8,10 @@ export const createContactSchema = Joi.object({
   contactType: Joi.string().valid('work', 'home', 'personal').default('personal'),
 });
 
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  phoneNumber: Joi.string().min(6).max(16).required(),
+  email: Joi.string(),
+  isFavorite: Joi.boolean().default(false),
+  contactType: Joi.string().valid('work', 'home', 'personal').default('personal'),
+});
