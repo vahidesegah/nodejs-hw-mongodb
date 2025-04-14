b5bc4593f337dcaafa2ce2a19cb0b4b6d210450e
Vahide Segah Üney <vahidesegah@gmail.com>
2025-04-14 14:51:30 +0300

    left position

--- src/constants/pagination.js
+++ src/constants/pagination.js
@@ -1,15 +1,19 @@
-export const DEFAULT_PAGINATION_VALUES = {
-    page: 1,
-    perPage: 10,
-};
-
 export const SORT_ORDER = {
     ASC: `asc`,
     DSC: `desc`,
 };
 
-export const SORT_BY = [
-    "id",
+
+export const DEFAULT_PAGINATION_VALUES = {
+    page: 1,
+    perPage: 10,
+    sortOrder: SORT_ORDER.ASC,
+    sortBy: "_id",
+};
+
+
+export const CONTACT_SORT_FIELDS = [
+    "_id",
     "name",
     "phoneNumber",
     "email",
--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,1 +1,17 @@
-export const parseSortParams = (query) = {};
+import { DEFAULT_PAGINATION_VALUES, SORT_ORDER, CONTACT_SORT_FIELDS } from "../constants/pagination.js";
+
+export const parseSortParams = (query) => {
+    const knownSortOrders = [SORT_ORDER.ASC, SORT_ORDER.DESC];
+    const sortOrder = query.sortOrder;
+    const isValidSortOrder = knownSortOrders.includes(sortOrder);
+
+    const sortBy = query.sortBy;
+    const isValidSortBy = CONTACT_SORT_FIELDS.includes(sortBy);
+
+    return {
+        sortOrder: isValidSortOrder ? sortOrder : DEFAULT_PAGINATION_VALUES.sortOrder,
+        sortBy: isValidSortBy ? sortBy : DEFAULT_PAGINATION_VALUES.sortBy,
+    };
+};
+
+
