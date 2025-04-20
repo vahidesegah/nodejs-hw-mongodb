b222fc5498748d106b19183e5b72a1ab9fcc7fca
Vahide Segah Üney <vahidesegah@gmail.com>
2025-04-20 11:38:27 +0300

    create user Schema added

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -1,5 +1,11 @@
 import Joi from 'joi';
 
+export const createUserSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).max(30).required(),
+});
+
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
