3e61368269c6db488f5a6f95131aebae111109b7
Vahide Segah Üney <vahidesegah@gmail.com>
2025-04-21 15:53:39 +0300

    requestResetEmailSchema

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -17,5 +17,9 @@ export const registerUserSchema = Joi.object({
   password: Joi.string().min(6).max(30).required(),
 });
 
+export const requestResetEmailSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+
 
 
