b7a67df10f7c980dd33cbfc9f9280a93b40c8a05
Vahide Segah Üney <vahidesegah@gmail.com>
2025-04-29 21:46:07 +0300

    sx

--- src/utils/googleOAuth.js
+++ src/utils/googleOAuth.js
@@ -4,7 +4,7 @@ import { OAuth2Client } from 'google-auth-library';
 import path from 'node:path';
 import { readFile } from 'fs/promises';
 import createHttpError from 'http-errors';
-import { env } from "../env.js";
+// import { env } from "../env.js";
 
 
 const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
@@ -12,8 +12,8 @@ const parsedOAuthConfig = JSON.parse(await readFile(PATH_JSON));
 
 
 const googleOAuthClient = new OAuth2Client({
-  clientId: env(`GOOGLE_AUTH_CLIENT_ID`), // çevresel değişkenleri değiştiriyoruz
-  clientSecret: env(`GOOGLE_AUTH_CLIENT_SECRET`), // çevresel değişkenleri değiştiriyoruz
+  clientId: process.env[`GOOGLE_AUTH_CLIENT_ID`], // çevresel değişkenleri değiştiriyoruz
+  clientSecret: process.env[`GOOGLE_AUTH_CLIENT_SECRET`], // çevresel değişkenleri değiştiriyoruz
   redirectUri: parsedOAuthConfig.web.redirect_uris[0],
 });
 
